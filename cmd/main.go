8ae5cc5a2021d0924611279a9ebba723e54d3c93
Mohamad Rizal Nurochman <muhamadrizalnurohman072005@gmail.com>
2026-06-30 06:10:49 +0700

    Refactor main.go to implement server run logic and update dependencies in go.mod and go.sum

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main()  {
-	
+import (
+	"log"
+	"os"
+
+	"github.com/common-nighthawk/go-figure"
+	"github.com/gin-gonic/gin"
+)
+
+func run(server *gin.Engine) {
+	port := os.Getenv("GOLANG_PORT")
+	if port == "" {
+		port = "8888"
+	}
+
+	var serve string
+	if os.Getenv("APP_ENV") == "localhost" {
+		serve = "0.0.0.0:" + port
+	} else {
+		serve = ":" + port
+	}
+
+	myFigure := figure.NewColorFigure("TEDxUniversitasAirlangga", "doom", "red", true)
+	myFigure.Print()
+
+	err := server.Run(serve)
+	if err != nil {
+		log.Fatalf("error running server: %s", err)
+	}
+}
+
+func main() {
+	server := gin.Default()
+
+	run(server)
 }
